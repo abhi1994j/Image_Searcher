d12a7c11a496f5ad6fab895cba2df3b6b585a14e
abhi1994j <abhibaban4@gmail.com>
2025-02-04 16:07:15 +0530

    create functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const searchInput = document.querySelector("#search-input");
 const accessKey = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
 // console.log(searchInput.value);
 const fromE1 = document.querySelector("form");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.querySelector("#show-more-button");
 // const searchBtn = doc
 let page = 1;
 let inputValue = ''
@@ -11,18 +13,32 @@ const searchImage =async()=>{
     const response = await fetch(url);
     // console.log(response);
     const res = await response.json();
-    console.log(res);
+    // console.log(res);
+    if(page === 1){
+        searchResults.innerHTML = "";
+    }
     const finalDB = res.results;
     finalDB.map((ele)=>{
-        const imageBox = document.createElement('div');
-        
-
-        
+        console.log(ele);
+        const imageBox = document.createElement("div");
+        imageBox.classList.add("search-result");
+        imageBox.innerHTML = `
+            <img src="${ele?.urls?.small}" alt="{ele?.alt_description}" /> <a href="${ele?.links?.html}">${ ele?.alt_description}</a>`
+        searchResults.appendChild(imageBox)   
     })
+    page++;
+    if(page>1){
+        showMore.style.display = "block";
+    }
 }
 fromE1.addEventListener("submit" , (e)=>{
     // console.log(e.target.value);
     e.preventDefault();
+    page = 1;
+    searchImage();
+})
+
+showMore.addEventListener("click" , ()=>{
     searchImage();
 })
 
